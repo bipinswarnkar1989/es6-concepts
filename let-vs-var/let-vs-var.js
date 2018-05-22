553626bab0f9947318d6ffccd5ef63d0f139e409
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-05-22 12:12:57 +0530

    let-vs-var using block scope redefined

--- let-vs-var/let-vs-var.js
+++ let-vs-var/let-vs-var.js
@@ -2,11 +2,12 @@ let letVariable = "letVariableValue";
 var varVariable = "varVariableValue";
 
 {
-     let letVariable = "letVariableValue-New";
-     varVariable = "varVariableValue-New";
+    let letVariable = "letVariableValueNew";
+    var varVariable = "varVariableValueNew";
     console.log(letVariable);
     console.log(varVariable);
 }
 
 console.log(letVariable);
 console.log(varVariable);
+
