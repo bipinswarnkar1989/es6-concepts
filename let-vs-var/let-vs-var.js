dd46a39ad7aef396708bc31f072779d3a6170cfe
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-05-22 11:51:01 +0530

    let vs var

---
+++ let-vs-var/let-vs-var.js
@@ -0,0 +1,12 @@
+let letVariable = "letVariableValue";
+var varVariable = "varVariableValue";
+
+{
+     let letVariable = "letVariableValue-New";
+     varVariable = "varVariableValue-New";
+    console.log(letVariable);
+    console.log(varVariable);
+}
+
+console.log(letVariable);
+console.log(varVariable);
